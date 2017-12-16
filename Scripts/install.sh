d6d78b649e4a0541ccc261bcc9287b5c273606d4
Alexander Koss <alexander@koss-solutions.de>
2017-12-16 13:12:18 +0100

    Add travis scripts & configuration

---
+++ Scripts/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+echo $(pwd)
+echo 'Installing Unity on Linux'
+
+echo "travis_fold:start:install_unity"
+echo 'Installing Unity'
+# latest Linux Unity details can be found at https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
+curl -o unity.deb http://beta.unity3d.com/download/ee86734cf592/unity-editor_amd64-2017.2.0f3.deb
+# from http://askubuntu.com/a/841240/310789
+sudo dpkg -i unity.deb
+echo "travis_fold:end:install_unity"
+
+echo "travis_fold:start:install_missing_dependencies"
+echo 'Installing missing dependencies'
+sudo apt-get install -f
+echo "travis_fold:end:install_missing_dependencies"
