2a43a9302244dca5951a1b983d3d2f2fd45dc8ac
Alexander Koss <alexander@koss-solutions.de>
2017-12-18 18:57:18 +0100

    Change upload script

--- Scripts/upload.sh
+++ Scripts/upload.sh
@@ -7,10 +7,11 @@ PATH=/var/www/html/master/
 
 uploadBuilds() {
 	plattform=$1
-	rsync -avr $(pwd)/Builds/$1/** $USERNAME:$PASSWORD@$SERVER:$PATH/Builds/$1/*
+	deploypath=$USERNAME:$PASSWORD@$SERVER:$PATH/Builds/$1/
+	echo $deploypath
+	scp -r $(pwd)/Builds/$1/* $deploypath
 }
 
-echo $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini
 echo "travis_fold:start:upload_build"
 	echo 'Upload windows build'
 	uploadBuilds "Windows"
