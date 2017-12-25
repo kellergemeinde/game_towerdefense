682c868c038dd0ce304c27f242c62d09d013a80a
Fahersto <fabian.stotz@yahoo.de>
2017-12-25 18:19:07 +0100

    Added InputWrapper

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project{
+
+    public class Input{
+
+
+        private static Input instance;
+
+        private Input(){}
+
+        public static Input Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Input();
+                }
+                return instance;
+            }
+        }
+
+        public Vector3 MouseWorldPosition{
+            get{
+                RaycastHit hit;
+                UnityEngine.Camera cam = UnityEngine.Camera.main;
+                if (!cam) {
+                    Debug.LogError("Input.MouseWorldPosition: Main camera not found!");
+                    return Vector3.zero;
+                }
+                Ray ray = cam.ScreenPointToRay(UnityEngine.Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit)){
+                    return hit.point;
+                }
+                return Vector3.zero;
+            }
+        }
+
+        public Vector2 MouseScreenPosition{
+            get{
+                Vector3 MousePosition = UnityEngine.Input.mousePosition;
+                return new Vector2(MousePosition.x, MousePosition.y);
+            }
+        }
+   
+    }
+}
