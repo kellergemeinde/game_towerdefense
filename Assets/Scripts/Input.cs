383a7f6d78939af904a2d5724c2b5570eed0a611
KomischOo <frau-trash@gmx.de>
2017-12-26 17:21:06 +0100

    Introducing CameraController and improved InputWrapper

--- Assets/Scripts/Input.cs
+++ Assets/Scripts/Input.cs
@@ -2,45 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Project{
+namespace Project
+{
 
-    public class Input{
-
+    public class Input
+    {
         private static Input instance;
+
+        private Vector3 mouseWorldPosition;
 
         private Input(){}
 
-        public static Input Instance{
-            get{
-                if (instance == null){
+        public static Input Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
                     instance = new Input();
                 }
                 return instance;
             }
         }
 
-        public Vector3 MouseWorldPosition{
-            get{
-                RaycastHit hit;
-                UnityEngine.Camera cam = UnityEngine.Camera.main;
-                if (!cam) {
-                    Debug.LogError("Input.MouseWorldPosition: Main camera not found!");
-                    return Vector3.zero;
-                }
-                Ray ray = cam.ScreenPointToRay(UnityEngine.Input.mousePosition);
-
-                if (Physics.Raycast(ray, out hit)){
-                    return hit.point;
-                }
-                return Vector3.zero;
+        public Vector3 MouseWorldPosition
+        {
+            get
+            {
+                return mouseWorldPosition;
             }
         }
 
-        public Vector2 MouseScreenPosition{
-            get{
-                Vector3 MousePosition = UnityEngine.Input.mousePosition;
-                return new Vector2(MousePosition.x, MousePosition.y);
+        public Vector2 MouseGridPosition
+        {
+            get
+            {
+                return new Vector2(mouseWorldPosition.x, mouseWorldPosition.y);
             }
+        }
+
+    
+        //This is no callback and has to be invoked by another script
+        public void Update()
+        {
+            RaycastHit hit;
+            UnityEngine.Camera cam = UnityEngine.Camera.main;
+            if (!cam)
+            {
+                Debug.LogError("Input.MouseWorldPosition: Main camera not found!");
+                mouseWorldPosition =  Vector3.zero;
+            }
+            Ray ray = cam.ScreenPointToRay(UnityEngine.Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                mouseWorldPosition =  hit.point;
+            }
+            mouseWorldPosition =  Vector3.zero;
         }
    
     }
