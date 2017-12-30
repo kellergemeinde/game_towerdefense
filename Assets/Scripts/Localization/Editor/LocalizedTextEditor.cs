5abb3d3a21bdcc5f4e803a55507f343721ade619
bl4ck <bl4ck@posteo.net>
2017-12-30 02:28:49 +0100

    Introducing LocalizationManager and a simple Text Editor for Localization Files (Windows->Localized Text Editor)

---
+++ Assets/Scripts/Localization/Editor/LocalizedTextEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class LocalizedTextEditor : EditorWindow
+{
+    public LocalizationData localizationData;
+
+    [MenuItem ("Window/Localized Text Editor")]
+    static void Init()
+    {
+        GetWindow(typeof(LocalizedTextEditor)).Show();
+    }
+
+    private void OnGUI()
+    {
+        if (localizationData != null)
+        {
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty serializedProperty = serializedObject.FindProperty("localizationData");
+            EditorGUILayout.PropertyField(serializedProperty, true);
+
+            serializedObject.ApplyModifiedProperties();
+
+            if (GUILayout.Button("Save data"))
+            {
+                SaveGameData();
+            }
+        }
+
+        if (GUILayout.Button("Load data"))
+        {
+            LoadGameData();
+        }
+
+        if (GUILayout.Button("Create new data"))
+        {
+            CreateNewData();
+        }
+    }
+
+    private void SaveGameData()
+    {
+        string filePath = EditorUtility.SaveFilePanel("Save localization data file", Path.Combine(Application.streamingAssetsPath,"Localization"), "", "json");
+
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            string dataAsJson = JsonUtility.ToJson(localizationData);
+            File.WriteAllText(filePath, dataAsJson);
+
+
+        }
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = EditorUtility.OpenFilePanel("Select localization data file", Path.Combine(Application.streamingAssetsPath, "Localization"), "json");
+
+        if (!string.IsNullOrEmpty(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            localizationData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+        }
+    }
+
+    private void CreateNewData()
+    {
+        localizationData = new LocalizationData();
+    }
+}
