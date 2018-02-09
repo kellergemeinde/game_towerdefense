ac18ae49a132e6594f4b89852dfe8061cf63ccb3
ROBIN-PC\Robin <bl4ck@posteo.net>
2018-02-10 00:52:29 +0100

    Changed to float [] cause of serialization Issues

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,27 +4,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+[Serializable]
 public class PlayerController : Project.NetworkBehaviour
 {
-    public Transform spawnLocation;
+    public static int MaxID { get; private set; }
+
+    [SerializeField]
+    public float[] Destination;
+
+    public Transform SpawnLocation;
     public GameObject workerPrefab;
     public GameObject soldierPrefab;
     public GameObject archerPrefab;
 
+    public int ID { get; private set; }
+    private GameObject PlayerUnitsEmpty;
+
     private static SpawnManager SpawnManager;
+
+    private List<Transform> LaneShields;
 
     // Use this for initialization
     void Start ()
     {
+        MaxID++;
+        ID = MaxID;
+
         if (SpawnManager == null)
             SpawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        SpawnManager.SpawnUnit += SpawnUnit;
 
-        SpawnManager.SpawnWorker += SpawnWorker;
-        SpawnManager.SpawnArcher += SpawnArcher;
-        SpawnManager.SpawnSoldier += SpawnSoldier;
+        if (SpawnLocation == null)
+            SpawnLocation = GameObject.Find("Level/Player" + ID + "/Location/Spawn").transform;
 
-        if (spawnLocation == null)
-            spawnLocation = GameObject.Find("Spawn").transform;
+        PlayerUnitsEmpty = new GameObject() { name = "Player" + ID };
+        PlayerUnitsEmpty.transform.parent = GameObject.Find("Units").transform;
+
+        LaneShields = new List<Transform>();
+        foreach (Transform shield in GameObject.Find("Level/Player" + ID + "/Shields").transform)
+        {
+            LaneShields.Add(shield.transform);
+        }
     }
 	
 	// Update is called once per frame
@@ -34,42 +54,54 @@ public class PlayerController : Project.NetworkBehaviour
             return;
 	}
 
-    private void SpawnWorker(object sender, EventArgs e)
+    private void SpawnUnit(SpawnManager.SpawnEventArgs e)
     {
-        CmdSpawnWorker();
-    }
+        if (!isLocalPlayer)
+            return;
 
-    private void SpawnArcher(object sender, EventArgs e)
-    {
-        CmdSpawnArcher();
-    }
+        Destination = new float[] { LaneShields[e.LaneToSpawn - 1].position.x + 0.5f, LaneShields[e.LaneToSpawn - 1].position.y, LaneShields[e.LaneToSpawn - 1].position.z };
 
-    private void SpawnSoldier(object sender, EventArgs e)
-    {
-        CmdSpawnSoldier();
+        switch (e.UnitToSpawn)
+        {
+            case SpawnManager.SpawnEventArgs.Unit.Worker:
+                CmdSpawnWorker(Destination);
+                break;
+            case SpawnManager.SpawnEventArgs.Unit.Archer:
+                CmdSpawnArcher(Destination);
+                break;
+            case SpawnManager.SpawnEventArgs.Unit.Soldier:
+                CmdSpawnSoldier(Destination);
+                break;
+            default:
+                throw new NotImplementedException();
+                //break;
+        }
     }
 
     [Command]
-    private void CmdSpawnWorker()
+    private void CmdSpawnWorker(float[] Destination)
     {
-        var go = Instantiate(workerPrefab, spawnLocation.position, Quaternion.identity);
+        var go = Instantiate(workerPrefab, SpawnLocation.position, Quaternion.identity, PlayerUnitsEmpty.transform);
         go.SendMessage("SetPlayer", this);
+        go.SendMessage("SetDestination", Destination);
         NetworkServer.Spawn(go);
     }
 
     [Command]
-    private void CmdSpawnArcher()
+    private void CmdSpawnArcher(float[] Destination)
     {
-        var go = Instantiate(archerPrefab, spawnLocation.position, Quaternion.identity);
+        var go = Instantiate(archerPrefab, SpawnLocation.position, Quaternion.identity, PlayerUnitsEmpty.transform);
         go.SendMessage("SetPlayer", this);
+        go.SendMessage("SetDestination", Destination);
         NetworkServer.Spawn(go);
     }
 
     [Command]
-    private void CmdSpawnSoldier()
+    private void CmdSpawnSoldier(float[] Destination)
     {
-        var go = Instantiate(soldierPrefab, spawnLocation.position, Quaternion.identity);
+        var go = Instantiate(soldierPrefab, SpawnLocation.position, Quaternion.identity, PlayerUnitsEmpty.transform);
         go.SendMessage("SetPlayer", this);
+        go.SendMessage("SetDestination", Destination);
         NetworkServer.Spawn(go);
     }
 }
