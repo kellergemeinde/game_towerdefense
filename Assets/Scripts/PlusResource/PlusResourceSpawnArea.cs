9ca3a1ebe9281af00d72786dd8555c07f66f5f8a
xdubx <klaus.boldt97@gmail.com>
2018-06-10 14:42:16 +0200

    update mid; work on wall3 ;set delay on spawn script;

--- Assets/Scripts/PlusResource/PlusResourceSpawnArea.cs
+++ Assets/Scripts/PlusResource/PlusResourceSpawnArea.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace Project.Entities
 {
@@ -17,20 +18,21 @@ namespace Project.Entities
         void Start()
         {
             plusSpawnArea = this.gameObject;
-            spawnResource();
+            StartCoroutine(spawnResource());
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            
         }
         //TODO add resource spawn event
-        private void spawnResource()
+        IEnumerator spawnResource()
         {
+            yield return new WaitForSeconds(5);
             GameObject resource = Instantiate(plusResourcePrefab, new Vector3(plusSpawnArea.transform.position.x, plusSpawnArea.transform.position.y, plusSpawnArea.transform.position.z), Quaternion.identity);
             resource.transform.SetParent(plusSpawnArea.transform);
-            resource.GetComponent<Rigidbody>().AddForce(plusSpawnArea.transform.up * 3, ForceMode.Impulse);
+            resource.GetComponent<Rigidbody>().AddForce(plusSpawnArea.transform.up * 4, ForceMode.Impulse);
         }
     }
 }
