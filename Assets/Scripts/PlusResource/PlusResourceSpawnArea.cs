8da219751f031ed21a6cdfaaf9a020d3811587da
xdubx <klaus.boldt97@gmail.com>
2018-06-10 04:11:36 +0200

    update map in unity; work on mid, fix wall 3 with to much faces; write some code at Resource Spawn script

--- Assets/Scripts/PlusResource/PlusResourceSpawnArea.cs
+++ Assets/Scripts/PlusResource/PlusResourceSpawnArea.cs
@@ -2,44 +2,35 @@
 
 namespace Project.Entities
 {
-    public class PlusResourceSpawnArea : NetworkBehaviour
+    public class PlusResourceSpawnArea : Behaviour
     {
+        public GameObject plusResourcePrefab;
         //[SerializeField]
         private int amountOfPlusAtTheBeginning;
 
         //[SerializeField]
         private GameObject plusSpawnArea;
 
-        //[SerializeField]
-        private GameObject plusResourcePrefab;
-
-        private RangeAttribute minMaxX;
-        private RangeAttribute minMaxY;
-        private RangeAttribute minMaxZ;
-
         /// <summary>
         /// Spawn the plus resource at the beginning of the game.
         /// </summary>
         void Start()
         {
-            Vector3 center = plusSpawnArea.transform.position;
-            Vector3 extents = plusSpawnArea.transform.localScale / 2;
-            minMaxY = new RangeAttribute(center.y - extents.y, center.y + extents.y);
-            minMaxZ = new RangeAttribute(center.z - extents.z, center.z + extents.z);
-
-            for (int i = 0; i < amountOfPlusAtTheBeginning; i++)
-            {
-                Instantiate(plusResourcePrefab, new Vector3(
-                    center.x, Random.Range(minMaxY.min, minMaxY.max), Random.Range(minMaxZ.min, minMaxZ.max)), 
-                    Quaternion.identity,
-                    GameObject.Find("Ressource").transform);
-            }
+            plusSpawnArea = this.gameObject;
+            spawnResource();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+        //TODO add resource spawn event
+        private void spawnResource()
+        {
+            GameObject resource = Instantiate(plusResourcePrefab, new Vector3(plusSpawnArea.transform.position.x, plusSpawnArea.transform.position.y, plusSpawnArea.transform.position.z), Quaternion.identity);
+            resource.transform.SetParent(plusSpawnArea.transform);
+            resource.GetComponent<Rigidbody>().AddForce(plusSpawnArea.transform.up * 3, ForceMode.Impulse);
         }
     }
 }
